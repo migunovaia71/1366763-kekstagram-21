4438a52b22f07fc174dfbee0a6b9635c6aeedf1f
migunovaia71 <migunovaia71@gmail.com>
2020-10-08 21:40:14 +0300

    Личный проект: модуляция

--- js/effects.js
+++ js/effects.js
@@ -1,49 +1,53 @@
-'use strict';
+(function () {
+  'use strict';
 
-const effectLevelPin = document.querySelector('.effect-level__pin');
-const effectLevelLine = document.querySelector('.effect-level__line');
-const defaultLevelValue = document.querySelector('.effect-level__value');
-const imgUploadEffectLevel = document.querySelector('.img-upload__effect-level');
-const effectsRadios = document.querySelectorAll('.effects__radio');
-const imgUploadPreview = document.querySelector('.img-upload__preview');
-const imgUploadForm = document.querySelector('.img-upload__form');
-const effectsList = document.querySelector('.effects__list');
-const defaultEffectRadio = document.querySelector('#effect-none');
+  const effectLevelPin = document.querySelector('.effect-level__pin');
+  const effectLevelLine = document.querySelector('.effect-level__line');
+  const defaultLevelValue = document.querySelector('.effect-level__value');
+  const imgUploadEffectLevel = document.querySelector('.img-upload__effect-level');
+  const effectsRadios = document.querySelectorAll('.effects__radio');
+  const imgUploadPreview = document.querySelector('.img-upload__preview');
+  const imgUploadForm = document.querySelector('.img-upload__form');
+  const effectsList = document.querySelector('.effects__list');
+  const defaultEffectRadio = document.querySelector('#effect-none');
 
-effectsList.addEventListener('click', () => {
-  const effectLevel = defaultLevelValue.value;
-});
+  effectsList.addEventListener('click', () => {
+    const effectLevel = defaultLevelValue.value;
+  });
 
-effectLevelPin.addEventListener('mouseup', () => {
-  const effectLevel = (effectLevelPin.offsetLeft / effectLevelLine.offsetWidth) * 100;
-});
+  effectLevelPin.addEventListener('mouseup', () => {
+    const effectLevel = (effectLevelPin.offsetLeft / effectLevelLine.offsetWidth) * 100;
+  });
 
-let lastEffectClass = '';
+  let lastEffectClass = '';
 
-const setDefaultEffect = () => {
-  defaultEffectRadio.checked = true;
-  setEffect(defaultEffectRadio);
-}
-
-const setEffect = (radio) => {
-  if (lastEffectClass !== '') {
-    imgUploadPreview.classList.remove(lastEffectClass);
-  }
-  if (radio.checked) {
-    if (radio.value !== 'none') {
-      const effectClass = 'effects__preview--' + radio.value;
-      lastEffectClass = effectClass;
-      imgUploadPreview.classList.add(effectClass);
-      imgUploadEffectLevel.classList.remove('hidden');
-    } else {
-      imgUploadEffectLevel.classList.add('hidden');
+  window.effects = {
+    setDefaultEffect: () => {
+      defaultEffectRadio.checked = true;
+      setEffect(defaultEffectRadio);
     }
   }
-}
 
-imgUploadForm.addEventListener('change', (evt) => {
-  if (evt.target.classList.contains('effects__radio')) {
-    const radio = evt.target;
-    setEffect(radio);
+  const setEffect = (radio) => {
+    if (lastEffectClass !== '') {
+      imgUploadPreview.classList.remove(lastEffectClass);
+    }
+    if (radio.checked) {
+      if (radio.value !== 'none') {
+        const effectClass = 'effects__preview--' + radio.value;
+        lastEffectClass = effectClass;
+        imgUploadPreview.classList.add(effectClass);
+        imgUploadEffectLevel.classList.remove('hidden');
+      } else {
+        imgUploadEffectLevel.classList.add('hidden');
+      }
+    }
   }
-});
+
+  imgUploadForm.addEventListener('change', (evt) => {
+    if (evt.target.classList.contains('effects__radio')) {
+      const radio = evt.target;
+      setEffect(radio);
+    }
+  });
+})();
--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,33 @@
-const KEY_ESCAPE = 'Escape';
+(function () {
+  const KEY_ESCAPE = 'Escape';
 
-const uploadFile = document.querySelector('#upload-file');
-const uploadOverlay = document.querySelector('.img-upload__overlay');
-const uploadCancel = uploadOverlay.querySelector('#upload-cancel');
-const body = document.querySelector('body');
+  const uploadFile = document.querySelector('#upload-file');
+  const uploadOverlay = document.querySelector('.img-upload__overlay');
+  const uploadCancel = uploadOverlay.querySelector('#upload-cancel');
+  const body = document.querySelector('body');
 
-uploadFile.addEventListener('change', () => {
-  uploadOverlay.classList.remove('hidden');
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', onPopupEscPress);
-});
+  uploadFile.addEventListener('change', () => {
+    uploadOverlay.classList.remove('hidden');
+    body.classList.add('modal-open');
+    document.addEventListener('keydown', onPopupEscPress);
+  });
 
-const onPopupEscPress = (evt) => {
-  if (evt.key === KEY_ESCAPE) {
-    evt.preventDefault();
-    closePopup();
+  const onPopupEscPress = (evt) => {
+    if (evt.key === KEY_ESCAPE) {
+      evt.preventDefault();
+      closePopup();
+    }
+  };
+
+  const closePopup = () => {
+      uploadOverlay.classList.add('hidden');
+      body.classList.remove('modal-open');
+      document.removeEventListener('keydown', onPopupEscPress);
+      window.scale.setImgScale();
+      window.effects.setDefaultEffect();
   }
-};
 
-const closePopup = () => {
-    uploadOverlay.classList.add('hidden');
-    body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onPopupEscPress);
-    setImgScale();
-    setDefaultEffect();
-}
-
-uploadCancel.addEventListener('click', () => {
-  closePopup();
-});
+  uploadCancel.addEventListener('click', () => {
+    closePopup();
+  });
+})();
