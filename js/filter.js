2601ea50a87681309788be33b8a03da7fffa73c1
migunovaia71 <migunovaia71@gmail.com>
2020-10-25 16:48:48 +0300

    Фильтрация

---
+++ js/filter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function () {
+
+  const imgFilters = document.querySelector('.img-filters');
+  const filterDefault = document.querySelector('#filter-default');
+  const filterRandom = document.querySelector('#filter-random');
+  const filterDiscussed = document.querySelector('#filter-discussed');
+  imgFilters.classList.remove('img-filters--inactive');
+
+  filterDefault.addEventListener('click', () => {
+
+  });
+
+})();
