8c8d64a22d09fd977a62c5fb253b598322e49b76
migunovaia71 <migunovaia71@gmail.com>
2020-11-01 15:16:30 +0300

    Полноэкранный режим для всех фото

--- js/data.js
+++ js/data.js
@@ -8,24 +8,25 @@
     .content
     .querySelector('.picture');
 
-  const renderPhoto = (photo) => {
+  const renderPhoto = (photo, index) => {
     const element = pictureTemplate.cloneNode(true);
     element.querySelector('.picture__img').src = photo.url;
     element.querySelector('.picture__likes').textContent = photo.likes;
     element.querySelector('.picture__comments').textContent = photo.comments.length;
-
+    element.setAttribute('data-index', index);
     return element;
   }
 
   const pullDocument = (photos) => {
     const fragment = document.createDocumentFragment();
     for (let i = 0; i < photos.length; i++) {
-      fragment.appendChild(renderPhoto(photos[i]));
+      fragment.appendChild(renderPhoto(photos[i], i));
     }
     picturesElement.querySelectorAll('.picture').forEach((element) => {
       element.remove();
     })
     picturesElement.appendChild(fragment);
+    window.data.renderData = photos;
   }
 
   const errorHandler = (errorMessage) => {
@@ -36,13 +37,26 @@
     window.data.loadedData = data;
     pullDocument(data);
     window.filter.showFilters();
-    window.popup.open(window.data.loadedData[0]);
   }
 
   window.load.loadData(URL, successHandler, errorHandler);
 
+  const onPhotoClick = (event) => {
+    const clickElement = event.target;
+    const photoElement = clickElement.closest('.picture');
+    if (photoElement) {
+      const index = photoElement.getAttribute('data-index');
+      const photoData = window.data.renderData[index];
+      window.popup.open(photoData);
+    }
+    
+  }
+
+  picturesElement.addEventListener('click', onPhotoClick);
+
   window.data = {
     loadedData: [],
+    renderData: [],
     pullDocument: pullDocument
   };
 
